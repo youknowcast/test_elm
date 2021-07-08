dc7ba0ac52cb3d68d81b2ff7d555d34148121c44
Ambe Hideki <ykc@daycrift.net>
2021-07-08 23:33:40 +0900

    Add validation(plactice)

--- src/Form.elm
+++ src/Form.elm
@@ -1,9 +1,11 @@
 module Form exposing (..)
 
 import Browser
+import Char exposing (isDigit, isLower, isUpper)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
+import String exposing (any, length)
 
 
 
@@ -75,8 +77,8 @@ viewInput t p v toMsg =
 
 viewValidation : Model -> Html msg
 viewValidation model =
-    if model.password == model.passwordAgain then
+    if model.password == model.passwordAgain && length model.password > 8 && any isDigit model.password && any isUpper model.password && any isLower model.password then
         div [ style "color" "green" ] [ text "OK" ]
 
     else
-        div [ style "color" "red" ] [ text "Passwords do not match!" ]
+        div [ style "color" "red" ] [ text "Passwords invalid" ]
